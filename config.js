8c432e0742ba89d1001dc790d0e484d155cc2991
Khac Man <khacmanle@gmail.com>
2017-12-25 03:35:07 +0700

    [Design patterns] [Creational] Abstract factory. add missing dependency

---
+++ config.js
@@ -0,0 +1,13 @@
+"use strict";
+const config = {
+    CONSTANTS: {
+        MESSAGES : {
+            ERROR: {
+                "MISSING_OVERIDE": "Method has not been implemented!"
+            }
+        }
+    }
+};
+Object.freeze(config);
+
+module.exports = config;
