c6d51dd8d61a3d3bdae73024512f2722953361cc
Khac Man <khacmanle@gmail.com>
2018-01-25 22:56:22 +0700

    [Functional] test memoize

--- functional/memoize.js
+++ functional/memoize.js
@@ -1,5 +1,6 @@
 "use strict";
 const crc = require("crc");
+
 function hash(args) {
     const string = JSON.stringify(args);
     return crc.crc32(string).toString(16);
@@ -8,7 +9,7 @@ function hash(args) {
 function memoize(fn, hashFn = hash) {
     const cache = new Map();
 
-    return function invoker(...args) {
+    function invoker(...args) {
         const key = hashFn(args);
         if (cache.has(key)) {
             return cache.get(key);
@@ -17,11 +18,11 @@ function memoize(fn, hashFn = hash) {
         const result = fn(...args);
         cache.set(key, result);
         return result;
-    };
+    }
+    invoker.cache = cache;
+    invoker.hash = hash;
+
+    return invoker;
 }
 
-const square = memoize((n) => {
-    return n*n;
-});
-
-console.log(`${square(2)}\n${square(3)}\n${square(2)}`);
+module.exports = memoize;
---
+++ functional/memoize.test.js
@@ -0,0 +1,37 @@
+"use strict";
+const BenchMark = require("benchmark");
+const memoize = require("./memoize");
+
+function rawFibonacci(n) {
+    if (n <= 1) {
+        return n;
+    }
+    return rawFibonacci(n-1) + rawFibonacci(n-2);
+}
+
+const fibonacci = memoize(n => fibonacci(n-1) + fibonacci(n-2));
+fibonacci.cache.set(fibonacci.hash([0]), 0);
+fibonacci.cache.set(fibonacci.hash([1]), 1);
+
+const unary = fn => arg => fn(arg);
+
+function invokeRaw() {
+    const list = [0, 1, 2, 5, 8, 9, 12, 6, 9, 22, 11, 18, 20, 25, 18];
+    list.forEach(unary(rawFibonacci));
+}
+function invokeMem() {
+    const list = [0, 1, 2, 5, 8, 9, 12, 6, 9, 22, 11, 18, 20, 25, 18];
+    list.forEach(unary(fibonacci));
+}
+
+const suite = new BenchMark.Suite;
+suite.add("raw", invokeRaw)
+    .add("memoized", invokeMem)
+    .on('cycle', function(event) {
+        console.log(String(event.target));
+    })
+    .on('complete', function() {
+        console.log('Fastest is ' + this.filter('fastest').map('name'));
+    })
+    // run async
+    .run({"async":true});
