4a50488cdcbcca5fdc3c1826290c5ace2d428076
Khac Man <khacmanle@gmail.com>
2018-02-21 00:16:56 +0700

    [Core] mutation

---
+++ core/functionMutation.js
@@ -0,0 +1,18 @@
+"use strict";
+let obj = {
+	a: "Hello A",
+	b: {
+		x: "Hello X"
+	}
+};
+
+console.log(obj.a);
+
+function mutate(input) {
+	input.a = "World A";
+}
+
+// mutate(obj);
+Object.assign(obj.b, {y: "hello Y"});
+console.log(obj);
+
