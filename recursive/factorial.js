689a57e22cd883dbb73bf493b52e33d3e51ba96b
Khac Man <khacmanle@gmail.com>
2018-11-16 23:27:46 +0800

    iteration factorial

--- recursive/factorial.js
+++ recursive/factorial.js
@@ -1,14 +1,30 @@
 module.exports = {
-    linearFactorial
+    linearFactorial,
+    iterFactorial
 };
 
 function linearFactorial(x) {
-    if (x < 0) {
-        throw "Input must be a positive integer!";
-    }
+    throwIfNegative(x);
+
     if (x <= 1) {
         return 1;
     }
 
     return x * linearFactorial(x-1);
+}
+
+function iterFactorial(x) {
+    throwIfNegative(x);
+
+    let result = 1;
+    for (let i = 1; i <= x; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+function throwIfNegative(x) {
+    if (x < 0) {
+        throw "Input must be a positive integer!";
+    }
 }
