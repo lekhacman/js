714f6793d3b032c969f6e9023502b14ef369f02f
Khac Man <khacmanle@gmail.com>
2018-11-17 22:19:58 +0800

    Iterative fibonacci compute

--- recursive/fibonacci.js
+++ recursive/fibonacci.js
@@ -1,4 +1,24 @@
-module.exports = { fibonacci };
+module.exports = { fibonacci, iterFibonacci };
+
+/**
+ * Iterative process
+ * @param x
+ * @return {number}
+ */
+function iterFibonacci(x) {
+    let result = 0;
+    let a = 0;
+    let b = 1;
+
+    for (let i = 1; i <= x; i++) {
+        result = a + b;
+
+        const mem = a;
+        a += b;
+        b = mem;
+    }
+    return result;
+}
 
 /**
  * Tree recursive process
--- recursive/fibonacci.spec.js
+++ recursive/fibonacci.spec.js
@@ -7,3 +7,12 @@ test("Fibonacci", () => {
     expect(fib.fibonacci(4)).toEqual(3);
     expect(fib.fibonacci(8)).toEqual(21);
 });
+
+test("Fibonacci iter", () => {
+    expect(fib.iterFibonacci(0)).toEqual(0);
+    expect(fib.iterFibonacci(1)).toEqual(1);
+    expect(fib.iterFibonacci(2)).toEqual(1);
+    expect(fib.iterFibonacci(3)).toEqual(2);
+    expect(fib.iterFibonacci(4)).toEqual(3);
+    expect(fib.iterFibonacci(8)).toEqual(21);
+});
