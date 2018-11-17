b60652ef7ed5dac0bdb75f47aa0660fe3d37777e
Khac Man <khacmanle@gmail.com>
2018-11-17 14:49:52 +0800

    Ackermann's function

---
+++ sicp/c1/ackermann.js
@@ -0,0 +1,24 @@
+module.exports = { ackermann, ack };
+
+function ackermann(x, y) {
+    if (x === 0) {
+        return y + 1;
+    } else if (y === 0) {
+        return ackermann(
+            x -1,
+            1
+        );
+    } else {
+        return ackermann(
+            x -1,
+            ackermann(
+                x,
+                y -1
+            )
+        );
+    }
+}
+
+function ack(m, n) {
+    return m === 0 ? n + 1 : ack(m - 1, n === 0  ? 1 : ack(m, n - 1));
+}
---
+++ sicp/c1/ackermann.spec.js
@@ -0,0 +1,43 @@
+const a = require("./ackermann");
+
+describe("Ackermann", () => {
+    test("Ackermann's function", () => {
+        const cases = [
+            {
+                x: 1,
+                y: 0,
+                expect: 0
+            },
+            {
+                x: 0,
+                y: 4,
+                expect: 8
+            },
+            {
+                x: 3,
+                y: 1,
+                expect: 2
+            },
+            // {
+            //     x: 1,
+            //     y: 10,
+            //     expect: 3
+            // },
+            // {
+            //     x: 2,
+            //     y: 4,
+            //     expect: 2
+            // },
+            // {
+            //     x: 3,
+            //     y: 3,
+            //     expect: 3
+            // }
+        ];
+
+        cases.forEach(c => {
+            // expect(a.ackermann(c.x, c.y)).toEqual(c.expect);
+            expect(a.ackermann(c.x, c.y)).toEqual(a.ack(c.x, c.y));
+        });
+    });
+});
