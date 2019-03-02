a30e016d61491e5fb3e4e446cfaa22ec9f0eaf6c
Khac Man <khacmanle@gmail.com>
2019-03-02 23:14:23 +0800

    [Array] reduce

--- functional/array.js
+++ functional/array.js
@@ -12,3 +12,28 @@ ManArray.prototype.forEach = function(fn) {
         fn(this.value[i], i);
     }
 };
+
+ManArray.prototype.reduce = function(fn, initialValue) {
+    let result;
+
+    const array = new ManArray(this.value);
+    result = initialValue !== undefined ? initialValue : array.shift();
+    array.forEach(item => {
+        result = fn(result, item);
+    });
+
+    return result;
+};
+
+ManArray.prototype.shift = function() {
+    if (this.value.length > 0) {
+        const newValue = [];
+        const returnValue = this.value[0];
+        for (let i = 1; i < this.value.length; i++) {
+            newValue.push(this.value[i]);
+        }
+        this.value = newValue;
+
+        return returnValue;
+    }
+};
--- functional/array.test.js
+++ functional/array.test.js
@@ -13,3 +13,21 @@ test(".forEach", () => {
         2: "Man"
     });
 });
+
+test(".reduce", () => {
+    const array = new ManArray([
+        {
+            value: 3
+        },
+        {
+            value: 4
+        },
+        {
+            value: 5
+        }
+    ]);
+    expect(array.reduce((acc, obj) => acc + obj.value, 0)).toEqual(12);
+
+    const another = new ManArray([3, 4, 5]);
+    expect(another.reduce((acc, value) => acc + value)).toEqual(12);
+});
