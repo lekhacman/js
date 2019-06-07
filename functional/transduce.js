7ce416646ca504567e401ecbc572454666dd3e02
Khac Man <khacmanle@gmail.com>
2019-06-07 23:24:54 +0800

    Transducing

---
+++ functional/transduce.js
@@ -0,0 +1,32 @@
+const R = require("ramda");
+
+const list = [
+    {
+        name: "Le",
+        active: true
+    },
+    {
+        name: "Khac",
+        active: false
+    },
+    {
+        name: "Man",
+        active: true
+    },
+    {
+        name: "An",
+        active: true
+    },
+    {
+        name: "Drew",
+        active: false
+    }
+];
+
+const transducer = R.compose(
+    R.filter(R.prop("active")),
+    R.map(R.prop("name"))
+);
+
+const result = R.transduce(transducer, R.flip(R.append), [], list);
+console.log(result);
